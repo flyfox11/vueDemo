288bdb57d4e6b65375dc9945c06de8eb97b342ab
os-huzp <os-huzp@jsfund.cn>
2016-12-08 17:43:31 +0800

    vue-server-render

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="message">{{msg}}</div>
+</template>
+<script>
+export default {
+  data () {
+   return {
+    msg: 'Welcome to Vue World!'
+    }
+  }
+}
+</script>
+<style>
+.message{
+   color:green;
+   font-size:56px;
+   font-weight:bold;
+}
+</style>
---
+++ main.js
@@ -0,0 +1,8 @@
+
+// import Vue from 'vue/dist/vue'
+import App from './app.vue'
+
+new Vue({
+    el: '#app',
+    components:{App}
+});
